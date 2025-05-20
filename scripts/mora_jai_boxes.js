2e14925edf733b77ebf30ce96a0aa1878e647e82
kan kawabata <kkawabat@asu.edu>
2025-05-19 22:25:23 -0700

    polish

--- scripts/mora_jai_boxes.js
+++ scripts/mora_jai_boxes.js
@@ -118,232 +118,232 @@ export const mora_jai_data = [
     solution: "8782992522336811119"
   },
   {
-    location: "Blueprints A1",
+    location: "Blueprints Conservatory (A1)",
     pattern: "black grey orange orange green orange orange green yellow green green green green",
     solution: "27688439978897"
   },
   {
-    location: "Blueprints A2",
+    location: "Blueprints Maid's Chamber (A2)",
     pattern: "pink pink pink orange orange orange violet violet violet violet violet violet violet",
     solution: "9646662848"
   },
   {
-    location: "Blueprints A3",
+    location: "Blueprints Entrance Hall (A3)",
     pattern: "yellow yellow yellow black yellow green grey blue grey yellow yellow yellow yellow",
     solution: "4256336497989146"
   },
   {
-    location: "Blueprints A4",
+    location: "Blueprints Pantry (A4)",
     pattern: "blue violet blue black blue violet blue violet violet blue blue blue blue",
     solution: "41254"
   },
   {
-    location: "Blueprints A5",
+    location: "Blueprints Storeroom (A5)",
     pattern: "black black black grey orange grey pink red pink red red red red",
     solution: "122444227"
   },
     {
-    location: "Blueprints B1",
+    location: "Blueprints Bedroom (B1)",
     pattern: "orange yellow orange green grey green blue green blue green green green green",
     solution: "279231"
   },
   {
-    location: "Blueprints B2",
+    location: "Blueprints Drawing Room (B2)",
     pattern: "violet violet violet black grey grey orange blue orange violet violet violet violet",
     solution: "846654"
   },
   {
-    location: "Blueprints B3",
+    location: "Blueprints Gallery (B3)",
     pattern: "grey black grey yellow blue yellow blue blue blue blue blue blue blue",
     solution: "866141"
   },
   {
-    location: "Blueprints B4",
+    location: "Blueprints Library (B4)",
     pattern: "pink orange orange blue blue pink orange orange pink blue blue blue blue",
     solution: "753535"
   },
   {
-    location: "Blueprints B5",
+    location: "Blueprints Cloister (B5)",
     pattern: "pink black pink orange grey orange pink red pink orange red orange red",
     solution: "8713553159"
   },
   {
-    location: "Blueprints C1",
+    location: "Blueprints Solarium (C1)",
     pattern: "black green black orange violet orange blue green blue green green green green",
     solution: "517684988971"
   },
   {
-    location: "Blueprints C2",
+    location: "Blueprints Hallway (C2)",
     pattern: "white white white yellow white black blue blue blue blue blue blue blue",
     solution: "66134561"
   },
   {
-    location: "Blueprints C3",
+    location: "Blueprints Dining Room (C3)",
     pattern: "pink orange pink orange blue orange pink orange pink orange orange orange orange",
     solution: "12115"
   },
   {
-    location: "Blueprints C4",
+    location: "Blueprints Observatory (C4)",
     pattern: "green blue grey black blue grey violet blue blue blue blue blue blue",
     solution: "433789922"
   },
   {
-    location: "Blueprints C5",
+    location: "Blueprints East Wing Hall (C5)",
     pattern: "violet white violet white white white blue pink blue blue blue blue blue",
     solution: "486272292922262626228222"
   },
   {
-    location: "Blueprints D1",
+    location: "Blueprints Nook (D1)",
     pattern: "red green orange black black black violet blue violet violet violet violet violet",
     solution: "2888925631562796689177893316723851"
   },
   {
-    location: "Blueprints D2",
+    location: "Blueprints Kitchen (D2)",
     pattern: "orange green orange yellow orange orange blue yellow orange orange orange orange orange",
     solution: "218541"
   },
   {
-    location: "Blueprints D3",
+    location: "Blueprints Corridor (D3)",
     pattern: "green yellow green blue orange blue blue orange blue blue blue blue blue",
     solution: "792223146"
   },
   {
-    location: "Blueprints D4",
+    location: "Blueprints Music Room (D4)",
     pattern: "pink blue blue blue blue violet green grey pink blue blue blue blue",
     solution: "6163672222"
   },
   {
-    location: "Blueprints D5",
+    location: "Blueprints Lavatory (D5)",
     pattern: "orange yellow orange green yellow green black yellow black green yellow green yellow",
     solution: "3322273816"
   },
   {
-    location: "Blueprints E1",
+    location: "Blueprints Billiards Room (E1)",
     pattern: "pink white pink blue grey blue pink red pink red red red red",
     solution: "786528713211393"
   },
   {
-    location: "Blueprints E2",
+    location: "Blueprints Trophy Room (E2)",
     pattern: "white red grey grey pink grey black red white white white white white",
     solution: ""
   },
   {
-    location: "Blueprints E3",
+    location: "Blueprints Foundation (E3)",
     pattern: "grey orange orange yellow orange black blue blue orange orange orange orange orange",
     solution: "6245246417"
   },
   {
-    location: "Blueprints E4",
+    location: "Blueprints Ballroom (E4)",
     pattern: "green white white black white black blue white blue blue blue blue blue",
     solution: "79475586772318552"
   },
   {
-    location: "Blueprints E5",
+    location: "Blueprints Spare Room (E5)",
     pattern: "black blue grey blue orange blue black orange yellow blue blue blue blue",
     solution: "171552146422"
   },
   {
-    location: "Blueprints F1",
+    location: "Blueprints West Wing Hall (F1)",
     pattern: "black green black grey orange grey violet violet violet violet violet violet violet",
     solution: "88722878897531"
   },
   {
-    location: "Blueprints F2",
+    location: "Blueprints Passageway (F2)",
     pattern: "pink white pink orange red orange violet white violet violet violet violet violet",
     solution: "5875643444974562442561"
   },
   {
-    location: "Blueprints F3",
+    location: "Blueprints Dark Room (F3)",
     pattern: "yellow orange yellow orange orange orange pink orange pink orange orange orange orange",
     solution: "21445551222"
   },
   {
-    location: "Blueprints F4",
+    location: "Blueprints Closet (F4)",
     pattern: "pink orange pink orange orange orange green orange green orange orange orange orange",
     solution: "875"
   },
   {
-    location: "Blueprints F5",
+    location: "Blueprints Parlor Room (F5)",
     pattern: "black blue black black green orange blue green blue green green green green",
     solution: "4517827219731"
   },
   {
-    location: "Blueprints G1",
+    location: "Blueprints Gymnasium (G1)",
     pattern: "pink white pink black red black grey grey grey red red red red",
     solution: "75289877"
   },
   {
-    location: "Blueprints G2",
+    location: "Blueprints Laundry Room (G2)",
     pattern: "pink orange violet orange grey grey pink blue violet violet violet violet violet",
     solution: "1478991227228455595"
   },
   {
-    location: "Blueprints G3",
+    location: "Blueprints Guest Bedroom (G3)",
     pattern: "yellow yellow yellow black black black grey green grey black yellow black yellow",
     solution: "22283351277836292"
   },
   {
-    location: "Blueprints G4",
+    location: "Blueprints Conference Room (G4)",
     pattern: "yellow yellow yellow black grey black orange green orange yellow yellow yellow yellow",
     solution: "2622882321"
   },
   {
-    location: "Blueprints G5",
+    location: "Blueprints Den (G5)",
     pattern: "grey pink grey orange green orange violet grey violet violet violet violet violet",
     solution: "887788984884968"
   },
   {
-    location: "Blueprints H1",
+    location: "Blueprints Foyer (H1)",
     pattern: "green green green grey orange orange blue grey violet green green green green",
     solution: "766749449"
   },
   {
-    location: "Blueprints H2",
+    location: "Blueprints Pool (H2)",
     pattern: "red white yellow blue green blue blue yellow blue red red red red",
     solution: "256564542287899"
   },
   {
-    location: "Blueprints H3",
+    location: "Blueprints Servant's Quarters (H3)",
     pattern: "grey grey grey black red black white grey yellow black black black black",
     solution: "364147346388"
   },
   {
-    location: "Blueprints H4",
+    location: "Blueprints Pump Room (H4)",
     pattern: "violet blue violet black green orange black green orange violet violet violet violet",
     solution: "4589156253971"
   },
   {
-    location: "Blueprints H5",
+    location: "Blueprints Furnace (H5)",
     pattern: "black grey grey white green white blue yellow orange blue blue blue blue",
     solution: "227565146412292"
   },
   {
-    location: "Blueprints I1",
+    location: "Blueprints Archives (I1)",
     pattern: "red grey black orange orange orange green grey violet orange red orange red",
     solution: "1199162399718987871"
   },
   {
-    location: "Blueprints I2",
+    location: "Blueprints Chapel (I2)",
     pattern: "blue orange blue black black orange blue orange blue orange orange orange orange",
     solution: "175582617"
   },
   {
-    location: "Blueprints I3",
+    location: "Blueprints Vestibule (I3)",
     pattern: "grey green grey orange black red black white violet black violet red orange",
     solution: "88212858351223868215839"
   },
   {
-    location: "Blueprints I4",
+    location: "Blueprints Coat Check (I4)",
     pattern: "yellow green yellow black black black blue green blue blue black black blue",
     solution: "33337735377812"
   },
   {
-    location: "Blueprints I5",
+    location: "Blueprints Aquarium (I5)",
     pattern: "pink grey yellow red yellow white orange blue black yellow yellow yellow yellow",
     solution: "744442544448443244"
   },
   {
-    location: "Blueprints 46th room",
+    location: "Blueprints 46th room (H5)",
     pattern: "orange pink orange grey green grey blue grey blue blue blue blue blue",
     solution: "8481688898148"
   },
